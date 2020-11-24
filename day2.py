c1ae3f41f8b8d13de6c6ea6a143193b97a030d12
Pushpanjali J V <67823806+anjalilajna@users.noreply.github.com>
2020-11-24 12:00:31 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,18 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b,c=100,100,100
+>>> print(a/10)
+10.0
+>>> print(b*50)
+5000
+>>> print(c+60)
+160
+>>> name="anjali"
+>>> print(name.replace(name[2],"G"))
+anGali
+>>> a,b=1,2.0
+>>> a=float(a)
+>>> b=int(b)
+>>> print(a,b)
+1.0 2
+>>> 
