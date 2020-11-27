810b2b84370192e262cee79880a6930f98a8096b
Pushpanjali J V <67823806+anjalilajna@users.noreply.github.com>
2020-11-27 11:25:53 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    print("Addition of two numbers is" ,(a+b)) #Addition of two numbers is 3
+def sub(c,d):
+    print("Subtraction of two numbers is ",(c-d)) #Subtraction of two numbers is 1
+def div(e,f):
+    print("Division of two numbers is ",(e/f)) #Division of two numbers is 2.0
+def mul(g,h):
+    print("Multiplication of two numbers is ",(g*h)) #Multiplication of two numbers is 4
+add(1,2)
+sub(4,3)
+div(6,3)
+mul(2,2)
+
+def covid(pn,bodytemp=98):
+    print('patient name is ',pn,'body temperature is ', bodytemp) #patient name is ramesh body temperature is 98
+covid('ramesh',)
